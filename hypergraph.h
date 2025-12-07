b26f18aaa0625e9c95e12add164d38c8d6147574
Rushil Shah <rushilnshah2@gmail.com>
2025-12-07 17:45:14 -0500

    Added input reader function for min-cut

--- hypergraph.h
+++ hypergraph.h
@@ -4,23 +4,80 @@
 
 #include<iostream>
 #include<vector>
+#include<fstream>
+#include<sstream>
+#include<random>
 
 #ifndef HYPERGRAPH_MIN_CUT_HYPERGRAPH_H
 #define HYPERGRAPH_MIN_CUT_HYPERGRAPH_H
 
 
 struct Hyperedge {
-    double weight{};
+    int weight{};
     std::vector<int> vertices;
+
+    std::string toString() const {
+        std::ostringstream oss;
+        oss << "Edge of weight " << weight << ", vertices: ";
+        for (int v : vertices)
+            oss << v << " ";
+        oss << "\n";
+        return oss.str();
+    }
 };
 
 struct Vertex {
     std::vector<int> incidentEdges;
+
+    std::string toString() const {
+        std::ostringstream oss;
+        oss << "Vertex with hyperedges:";
+        for (int e : incidentEdges)
+            oss << e << " ";
+        oss << "\n";
+        return oss.str();
+    }
 };
 
 struct Hypergraph {
     std::vector<Vertex> vertices;
     std::vector<Hyperedge> edges;
+
+    Hypergraph(const std::string& fileName) {
+        std::ifstream fin(fileName);
+
+        int numHyperedges, numVertices;
+        fin >> numHyperedges >> numVertices;
+
+        edges = std::vector<Hyperedge>(numHyperedges);
+        vertices = std::vector<Vertex>(numVertices);
+        std::string line;
+
+        // Generate the Randomness
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(1, 100);
+
+        std::getline(fin, line);
+
+        for (int i = 0; i < numHyperedges; i++) {
+            std::getline(fin, line);
+            std::istringstream iss(line);
+            std::vector<int> edgeVertices;
+
+            // Assigning the Edges in the Graph
+            int vertexInd;
+            while (iss >> vertexInd){
+                edgeVertices.push_back(vertexInd-1);
+
+                // Assigning the Vertices in the Graph
+                vertices[vertexInd-1].incidentEdges.push_back(i);
+            }
+            int randomWeight = distrib(gen);
+            edges[i].weight = randomWeight;
+            edges[i].vertices = edgeVertices;
+        }
+    };
 };
 
 
