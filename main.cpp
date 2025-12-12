e9e40959192b4197a7131855d1eaeb5b3b28df97
Rushil Shah <rushilnshah2@gmail.com>
2025-12-12 10:59:10 -0500

    added experiment 2

--- main.cpp
+++ main.cpp
@@ -1,162 +1,147 @@
-//#include <algorithm>
-//#include <cmath>
-//#include <cstdint>
-//#include <expected>
-//#include <iostream>
-//#include <numeric>
-//#include <random>
-//#include <string>
-//#include <vector>
-//#include<fstream>
-//#include<sstream>
-//#include <unordered_set>
-//#include "randomized.h"
-//#include <chrono>
-//#include <filesystem>
-////#include "deterministichypergraph.h"
-////#include "deterministicbenchmark.h"
-//
-//void experimentOneRandomized(){
-//    std::ofstream outputFile("outputs/exp1Randomized.txt");
-//    outputFile << "n m k #contractions #weight edges-cut" << std::endl;
-//    std::vector<int> sizes = {1000};
-//    for (int n: sizes){
-//        for (int m: sizes){
-//            outputFile << n << " " << m << " 5 ";
-//            Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), n, m, 5, 1);
-//            auto result = Amplify(H, 2, 2, 1);
-//            outputFile << result.totalContractions << " " << result.cutWeight << " " << result.numContractedEdges << std::endl;
-//        }
-//    }
-//    outputFile << std::endl;
-//    auto     start = std::chrono::high_resolution_clock::now();
-//
-//    for (int n: sizes){
-//        for (int m: sizes){
-//            int k = (int)n/2;
-//            outputFile << n << " " << m << " " << k << " ";
-//            Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), n, m, k, 1);
-//            auto result = Amplify(H, 2, 2, 1);
-//            outputFile << result.totalContractions << " " << result.cutWeight << " " << result.numContractedEdges << std::endl;
-//        }
-//    }
-//    auto     end = std::chrono::high_resolution_clock::now();
-//    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start) << std::endl;
-//    outputFile.close();
-//}
-//
-//void experimentTwoRandomized(){
-//    std::string path_to_dir = "/Users/rushil/Documents/MIT/Fall25/6.5220/hypergraph-min-cut/circuit_hypergraphs";
-//    std::ofstream outputFile("outputs/exp2Randomized.txt");
-//    outputFile << "file_name #contractions #weight edges-cut time" << std::endl;
-//    for (const auto& entry: std::filesystem::directory_iterator(path_to_dir)){
-//        std::string file_name = entry.path().filename();
-//        std::string full_file_name = path_to_dir+"/"+file_name;
-//        outputFile << file_name << " ";
-//        Hypergraph H{full_file_name};
-//        auto start = std::chrono::high_resolution_clock::now();
-//        auto result = Amplify(H, 2, 2, 1);
-//        auto end = std::chrono::high_resolution_clock::now();
-//        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-//        outputFile << result.totalContractions << " " << result.cutWeight << " " << result.numContractedEdges << " " << duration.count() << std::endl;
-//    }
-//    outputFile.close();
-//}
-////
-////void experimentOneDeterministic(){
-////    std::ofstream outputFile("outputs/exp1Deterministic.txt");
-////    outputFile << "Format: n k #weight" << std::endl;
-////    std::vector<int> sizes = {10, 50, 100, 500, 1000};
-////    for (int size: sizes){
-////        std::cout << "Running " << size << std::endl;
-////        outputFile << size << " 5 ";
-////        Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), size, size, 5, 1);
-////        auto cutWeight = deterministicMinCut(H);
-////        outputFile << cutWeight << std::endl;
-////    }
-////    outputFile << std::endl;
-////    for (int size: sizes){
-////        std::cout << "Running " << size << std::endl;
-////        int k = (int)size/2;
-////        outputFile << size << " " << k << " ";
-////        Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), size, size, k, 1);
-////        auto cutWeight = deterministicMinCut(H);
-////        outputFile << cutWeight << std::endl;
-////    }
-////}
-////
-////void experimentTwoDeterministic(){
-////    std::string path_to_dir = "/Users/rushil/Documents/MIT/Fall25/6.5220/hypergraph-min-cut/circuit_hypergraphs";
-////    std::ofstream outputFile("outputs/exp2Deterministic.txt");
-////    outputFile << "file_name #weight time" << std::endl;
-////    for (const auto& entry: std::filesystem::directory_iterator(path_to_dir)){
-////        std::string file_name = entry.path().filename();
-////        std::cout << "Running " << file_name << std::endl;
-////        std::string full_file_name = path_to_dir+"/"+file_name;
-////        outputFile << file_name << " ";
-////        Hypergraph H{full_file_name};
-////        auto start = std::chrono::high_resolution_clock::now();
-////        auto cutWeight = deterministicMinCut(H);
-////        auto end = std::chrono::high_resolution_clock::now();
-////        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-////        outputFile << cutWeight << " " << duration.count() << std::endl;
-////    }
-////    outputFile.close();
-////}
-//
-//int main() {
-//    // Hypergraph H;
-//    // H.edges = {
-//    //         {{0, 1, 2, 5}, 50},
-//    //         {{2, 3, 5}, 3},
-//    //         {{1, 3, 4, 6}, 10},
-//    //         {{0, 2, 4, 6}, 5},
-//    //         {{3, 5}, 7},
-//    // };
-//    // H.n = 7;
-//    // auto cutWeight = deterministicMinCut(H);
-//    // std::cout << cutWeight << std::endl;
-//
-//    // Hypergraph H2;
-//    // H2.edges = {
-//    //         {{0, 1, 2, 5}, 50},
-//    //         {{2, 3, 5}, 3},
-//    //         {{1, 3, 4, 6}, 10},
-//    //         {{0, 2, 4, 6}, 5},
-//    //         {{3, 5}, 7},
-//    // };
-//    // H2.n = 7;
-//
-//    // auto randomCutWeight = Amplify(H2, 2, 2, 1);
-//    // std::cout << randomCutWeight.cutWeight << std::endl;
-//
-//    // experimentTwoDeterministic();
-//    // experimentTwoRandomized();
-//
-//    experimentOneRandomized();
-//}
-
-
-#include "hypergraph.h"
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <expected>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <string>
+#include <vector>
+#include<fstream>
+#include<sstream>
+#include <unordered_set>
 #include "randomized.h"
+#include <chrono>
+#include <filesystem>
 #include "deterministic.h"
 
+void experimentOneRandomized(){
+    std::ofstream outputFile("outputs/exp1Randomized.txt");
+    outputFile << "n m k #contractions #weight run-time" << std::endl;
+    for (int n = 100; n <= 1000; n += 100){
+        for (int m = 100; m <= 1000; m += 100){
+            outputFile << n << " " << m << " 10 ";
+            Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), n, m, 10, 1);
+            auto start = std::chrono::high_resolution_clock::now();
+            auto result = randomizedMinKCut(H, 2, 2, 1, 1);
+            auto end = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            outputFile << result.totalContractions << " " << result.cutWeight << " " << result.totalRuntime << " " << duration.count() << std::endl;
+        }
+    }
+    outputFile << std::endl;
+    for (int n = 100; n <= 1000; n += 100){
+        for (int m = 100; m <= 1000; m += 100){
+            int k = (int)std::log(n);
+            outputFile << n << " " << m << " " << k << " ";
+            Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), n, m, k, 1);
+            auto start = std::chrono::high_resolution_clock::now();
+            auto result = randomizedMinKCut(H, 2, 2, 1, 1);
+            auto end = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            outputFile << result.totalContractions << " " << result.cutWeight << " " << result.totalRuntime << " " << duration.count() << std::endl;
+        }
+    }
+    outputFile.close();
+}
+
+void experimentTwoRandomized(){
+    std::string path_to_circuit = "/Users/rushil/Documents/MIT/Fall25/6.5220/hypergraph-min-cut/circuit_hypergraphs";
+    std::ofstream outputFile("outputs/exp2Randomized.txt");
+    outputFile << "file_name #contractions #weight edges-cut time" << std::endl;
+    for (const auto& entry: std::filesystem::directory_iterator(path_to_circuit)){
+        std::string file_name = entry.path().filename();
+        std::string full_file_name = path_to_circuit+"/"+file_name;
+        outputFile << file_name << " ";
+        Hypergraph H{full_file_name};
+        auto start = std::chrono::high_resolution_clock::now();
+        auto result = randomizedMinKCut(H, 2, 2, 1, 1);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        outputFile << result.totalContractions << " " << result.cutWeight << " " << result.totalRuntime << " " << duration.count() << std::endl;
+    }
+    outputFile << std::endl;
+    std::string path_to_hyperff = "/Users/rushil/Documents/MIT/Fall25/6.5220/hypergraph-min-cut/hyperff_hypergraphs";
+    for (const auto& entry: std::filesystem::directory_iterator(path_to_hyperff)){
+        std::string file_name = entry.path().filename();
+        std::cout << "Running " << file_name << std::endl;
+        std::string full_file_name = path_to_hyperff+"/"+file_name;
+        outputFile << file_name << " ";
+        Hypergraph H{full_file_name};
+        auto start = std::chrono::high_resolution_clock::now();
+        auto result = randomizedMinKCut(H, 2, 2, 1, 1);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        outputFile << result.totalContractions << " " << result.cutWeight << " " << result.totalRuntime << " " << duration.count() << std::endl;
+    }
+
+    outputFile.close();
+}
+
+void experimentOneDeterministic(){
+    std::ofstream outputFile("outputs/exp1Deterministic.txt");
+    outputFile << "n m k weight run-time" << std::endl;
+    std::vector<int> sizes = {1000};
+    for (int n = 100; n <= 1000; n += 100){
+        for (int m = 100; m <= 1000; m += 100){
+            outputFile << n << " " << m << " 10 ";
+            Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), n, m, 10, 1);
+            auto start = std::chrono::high_resolution_clock::now();
+            auto cutWeight = deterministicMinCut(H);
+            auto end = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            outputFile << cutWeight.cutWeight << " " << duration.count() << std::endl;
+        }
+    }
+    outputFile << std::endl;
+    for (int n = 100; n <= 1000; n += 100){
+        for (int m = 100; m <= 1000; m += 100){
+            int k = (int)log(n);
+            outputFile << n << " " << m << " " << k << " ";
+            Hypergraph H = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 100), n, m, k, 1);
+            auto start = std::chrono::high_resolution_clock::now();
+            auto cutWeight = deterministicMinCut(H);
+            auto end = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            outputFile << cutWeight.cutWeight << " " << duration.count() << std::endl;
+        }
+    }
+}
+
+void experimentTwoDeterministic(){
+    std::string path_to_circuit = "/Users/rushil/Documents/MIT/Fall25/6.5220/hypergraph-min-cut/circuit_hypergraphs";
+    std::ofstream outputFile("outputs/exp2Deterministic.txt");
+    outputFile << "file_name #weight time" << std::endl;
+    for (const auto& entry: std::filesystem::directory_iterator(path_to_circuit)){
+        std::string file_name = entry.path().filename();
+        std::cout << "Running " << file_name << std::endl;
+        std::string full_file_name = path_to_circuit+"/"+file_name;
+        outputFile << file_name << " ";
+        Hypergraph H{full_file_name};
+        auto start = std::chrono::high_resolution_clock::now();
+        auto result = deterministicMinCut(H);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        outputFile << result.cutWeight << " " << duration.count() << std::endl;
+    }
+    outputFile << std::endl;
+    std::string path_to_hyperff = "/Users/rushil/Documents/MIT/Fall25/6.5220/hypergraph-min-cut/hyperff_hypergraphs";
+    for (const auto& entry: std::filesystem::directory_iterator(path_to_hyperff)){
+        std::string file_name = entry.path().filename();
+        std::cout << "Running " << file_name << std::endl;
+        std::string full_file_name = path_to_hyperff+"/"+file_name;
+        outputFile << file_name << " ";
+        Hypergraph H{full_file_name};
+        auto start = std::chrono::high_resolution_clock::now();
+        auto result = deterministicMinCut(H);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        outputFile << result.cutWeight << " " << duration.count() << std::endl;
+    }
+
+    outputFile.close();
+}
 
 int main() {
-    uint64_t seed = 2;
-    auto h = kUniformHypergraph(std::uniform_int_distribution<uint32_t>(1, 1), 1000, 1000, 5, seed);
-
-    auto randomizedStart = std::chrono::high_resolution_clock::now();
-    auto randomizedCut = randomizedMinKCut(h, 2, 2, 2, seed);
-    auto randomizedEnd = std::chrono::high_resolution_clock::now();
-
-    auto deterministicStart = std::chrono::high_resolution_clock::now();
-    auto deterministicCut = deterministicMinKCut(h, 2);
-    auto deterministicEnd = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Randomized runtime: " << std::chrono::duration_cast<std::chrono::milliseconds>(randomizedEnd - randomizedStart) << std::endl;
-    std::cout << "Deterministic runtime: " << std::chrono::duration_cast<std::chrono::milliseconds>(deterministicEnd - deterministicStart) << std::endl;
-
-    std::cout << "Randomized cut value: " << randomizedCut.cutWeight << std::endl;
-    std::cout << "Deterministic cut value: " << deterministicCut.cutWeight << std::endl;
+    experimentOneRandomized();
 }
--- plotting.py
+++ plotting.py
@@ -6,33 +6,138 @@ def exp2_generate_time_comp():
     randomized = open("./outputs/exp2Randomized.txt", "r")
     plot_names = ["c17", "c432", "c499", "c880", "c1355", "c1908", "c2670", "c3540", "c5315", "c6288", "c7552"]
 
-    deterministic_times = [0]*len(plot_names)
-    randomized_times = [0]*len(plot_names)
+    deterministic_times = [10]*len(plot_names)
+    randomized_times = [10]*len(plot_names)
+    
+    hyperff_sizes = [0]*10
+    deterministic_hyperff_times = [0]*10
+    randomized_hyperff_times = [0]*10
+    
+    next_type = False
+    counter = 0
     
     for line in deterministic.readlines():
-        split_line = line.split(" ")
-        name = split_line[0].split(".")[0]
-        idx = plot_names.index(name)
-        deterministic_times[idx] = int(split_line[3])/(10**6)
+        if line[0:9] == "file_name":
+            continue
+        if line == "\n":
+            next_type = True
+            continue
+        if not next_type:
+            split_line = line.split(" ")
+            name = split_line[0].split(".")[0]
+            idx = plot_names.index(name)
+            deterministic_times[idx] = int(split_line[-1])/(10**6)
+        else:
+            split_line = line.split(" ")
+            name = split_line[0].split(".")[0]
+            size = int(name[5])
+            deterministic_hyperff_times[size] = int(split_line[-1])/(10**6)
+            hyperff_sizes[size] = 500+size*100
     
+    next_type = False
     for line in randomized.readlines():
-        split_line = line.split(" ")
-        name = split_line[0].split(".")[0]
-        idx = plot_names.index(name)
-        randomized_times[idx] = int(split_line[3])/(10**6)
+        if line[0:9] == "file_name":
+            continue
+        if line == "\n":
+            next_type = True
+            continue
+        if not next_type:
+            split_line = line.split(" ")
+            name = split_line[0].split(".")[0]
+            idx = plot_names.index(name)
+            randomized_times[idx] = int(split_line[-1])/(10**6)
+        else:
+            split_line = line.split(" ")
+            name = split_line[0].split(".")[0]
+            size = int(name[5])
+            randomized_hyperff_times[size] = int(split_line[-1])/(10**6)
 
     bar_width = 0.35
     x = np.arange(len(plot_names))
-    plt.bar(x - bar_width/2, deterministic_times, bar_width, label='KW Times')
-    plt.bar(x + bar_width/2, randomized_times, bar_width, label='FPZ Times')
     
-    plt.xlabel('Circuit Graphs')
-    plt.ylabel('Time (s)')
-    plt.title('Time of Running Circuit Hypergraphs on KW vs. FPZ')
-    plt.xticks(x, plot_names)
-    plt.legend()
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+    axs[0].bar(x - bar_width/2, deterministic_times, bar_width, label='KW Times')
+    axs[0].bar(x + bar_width/2, randomized_times, bar_width, label='FPZ Times')
+    
+    axs[0].set_xlabel('Circuit Graphs')
+    axs[0].set_ylabel('Time (s)')
+    axs[0].set_title('Time of Running Circuit Hypergraphs on KW vs. FPZ')
+    axs[0].set_xticks(x, plot_names)
+    axs[0].set_yscale('log')
+    axs[0].legend()
+    
+    x = np.arange(len(hyperff_sizes))
+    
+    axs[1].bar(x - bar_width/2, deterministic_hyperff_times, bar_width, label='KW Times')
+    axs[1].bar(x + bar_width/2, randomized_hyperff_times, bar_width, label='FPZ Times')
+    
+    axs[1].set_xlabel('Number of Nodes w/ (p = 0.5, q = 0.2)')
+    axs[1].set_ylabel('Time (s)')
+    axs[1].set_title('Time of Running HyperFF Hypergraphs on KW vs. FPZ')
+    axs[1].set_xticks(x, hyperff_sizes)
+    axs[1].set_yscale('log')
+    axs[1].legend()
+    
+    plt.tight_layout()
+    plt.show()  
+    
+def exp1_generate_plots():
+    data = open("./outputs/exp1Randomized.txt", "r")
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 4), subplot_kw={'projection': '3d'})
+    
+    next_graph = False
+    x = np.linspace(100, 1000, num=10)
+    y = np.linspace(100, 1000, num=10)
+    X, Y = np.meshgrid(x, y)
+    Z1 = np.zeros(X.shape)
+    Z2 = np.zeros(X.shape)
+    counter = 0
+    
+    for line in data.readlines():
+        if line[0] == "n":
+            continue
+        if len(line) == 1:
+            next_graph = True
+            counter = 0
+            continue
+        if next_graph:
+            split_line = np.array(line.split(" "))
+            print(split_line)
+            n, m, k = split_line[0:3].astype(int)
+            units = int(split_line[-1][:-2])
+            print(units)
+            Z2[counter//10][counter%10] = units
+        else:
+            split_line = np.array(line.split(" "))
+            n, m, k = split_line[0:3].astype(int)
+            units = int(split_line[-1][:-2])
+            print(units)
+            Z1[counter//10][counter%10] = units
+        counter += 1
+
+    axs[1].set_title('k = ln n Runtimes')
+    axs[1].set_xlabel('Number of Nodes')
+    axs[1].set_ylabel('Number of Edges')
+    axs[1].set_zlabel('Total Time (units)')
+
+    axs[0].set_title('k = 10 Runtimes')
+    axs[0].set_xlabel('Number of Nodes')
+    axs[0].set_ylabel('Number of Edges')
+    axs[0].set_zlabel('Total Time (units)')
+    
+    axs[0].plot_surface(X, Y, Z1)
+    axs[1].plot_surface(X, Y, Z2)
+    
     plt.tight_layout()
     plt.show()
-    
+
+# exp1_generate_plots()
+exp2_generate_time_comp()
+
+# 3D (n, m) graph for randomized to show asymptotic bounds. Two graphs, k = 10, k = ln n. Do same for deterministic but plot time, not units. That plot will also have randomized and deterministic.
+# exp2_time_comp(): Two comparison graphs, one for circuits and one for hyperff
+# 
 
 
+# In future: Go for m = n^2, try on many seeds and plot
